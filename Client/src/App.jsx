46aba0a6f1e72a70595bcb175ca2f94bad08284d
Asham0711 <mdashamimad3246@gmail.com>
2024-09-02 15:20:06 +0530

    Created initial folders for Client and Server

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center bg-gray-400 text-4xl font-bold">
+        Nexisa
+      </div>
+    </>
+  )
+}
+
+export default App
