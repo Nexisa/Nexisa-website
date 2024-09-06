cb1a948fb9728332a2e3c7743d2845226f1e50e4
Asham0711 <mdashamimad3246@gmail.com>
2024-09-06 23:32:35 +0530

    Edited service and hero section

--- Client/src/components/HeroSection.jsx
+++ Client/src/components/HeroSection.jsx
@@ -4,14 +4,14 @@ import heroImg from '../assets/Logo/heroImg.png';
 
 function HeroSection() {
   return (
-    <section className="max-w-screen bg-[#5846F9] py-16 h-screen">
+    <section className="max-w-screen bg-[#5846F9] py-4 h-screen">
       {/* <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-black/70"></div> */}
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center px-4 sm:px-6 lg:px-8 h-full">
         <div className="md:w-1/2 text-center md:text-left mb-8 md:mb-0">
-          <h1 className="text-5xl font-bold text-white leading-tight">
+          <h1 className="mt-4 md:mt-0 text-3xl md:text-5xl font-bold text-white leading-tight">
             Better Digital Experience With <span className="text-white">NEXISA</span>
           </h1>
-          <p className="mt-16 text-white text-lg">
+          <p className="mt-8 md:mt-16 text-white md:text-lg">
             Nexisa offers innovative IT solutions and expert training to empower businesses and individuals in the digital age.
           </p>
         </div>
@@ -19,7 +19,7 @@ function HeroSection() {
           <img
             src={heroImg} 
             alt="Digital Experience"
-            className="w-{100%} h-auto animate-float ml-25"
+            className="w-{100%} h-96 animate-float ml-25"
           />
         </div>
       </div>
