9001e0f4600f2b6248317989e3aa8a4f0a679948
Asham0711 <mdashamimad3246@gmail.com>
2024-10-01 13:56:49 +0530

    Added server file and initiated backend

---
+++ Server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("dotenv").config();
+// const connectDB = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//********************************database connection********************************
+// connectDB();
+
+//********************************middlewares********************************
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+
+//********************************start server********************************
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`App is running successfully at ${PORT}`);
+});
