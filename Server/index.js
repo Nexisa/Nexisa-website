d5c8f33482bfebb3c262a55b1eb3fd955b6927fd
Aayush Kumar <ayushbhardwaj9504@gmail.com>
2024-10-02 14:25:59 +0530

    checking my commit

--- Server/index.js
+++ Server/index.js
@@ -18,9 +18,8 @@ app.use(
   })
 );
 
-
 //********************************start server********************************
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`App is running successfully at ${PORT}`);
+  console.log(`App is running successfully PORT: ${PORT}`);
 });
