a5f7c4a5a85a23a956ad343a129af42aac2f8c6c
Aayush Kumar <ayushbhardwaj9504@gmail.com>
2024-10-04 10:39:06 +0530

    some more routes added as per the requirements

--- Server/routes/admin.routes.js
+++ Server/routes/admin.routes.js
@@ -4,10 +4,13 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const {
   getAllEmployees,
+  getEmployeeById,
   getAllLeaveApplications,
   manageLeaveApplication,
+  getLeaveApplicationById,
   addSalarySlip,
   addEmployee,
+  deleteEmployee,
 } = require("../controllers/admin.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const router = express.Router();
@@ -18,16 +21,25 @@ router.use(authMiddleware(["admin"]));
 //route for getting all employees
 router.get("/employees", getAllEmployees);
 
+//route for getting a specific employee by ID
+router.get("/employees/:id", getEmployeeById);
+
 // route for getting all leave applications
 router.get("/leave-applications", getAllLeaveApplications);
 
 // route for managing leave application
 router.put("/manage-leave", manageLeaveApplication);
 
+// get leave application by it's id
+router.get("/leave-applications/:id", getLeaveApplicationById);
+
 // route for adding salary slip
 router.post("/add-salary-slip", upload.single("fileUrl"), addSalarySlip);
 
 // route for adding employee
 router.post("/add-employee", addEmployee);
+
+// route for deleting employee
+router.delete("/employees/:id", deleteEmployee);
 
 module.exports = router;
