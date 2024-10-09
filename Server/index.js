95bd37b9e4676ed705f8cf3230969ddf3ac7e656
sahel <sahel22.vis@gmail.com>
2024-10-09 13:12:39 +0530

    me dumb part 2

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://nexisa-backend.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
