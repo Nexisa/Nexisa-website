9d498cda1ecb2679f230cd6f15cabb6437ea3c62
sahel <sahel22.vis@gmail.com>
2024-10-09 22:42:17 +0530

    ok

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://nexisa-backend.vercel.app/api",
+  baseURL: "https://nexisa-website.vercel.app/api",
 });
 
 export default instance;
